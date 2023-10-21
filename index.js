f2db8cfb3c70ae0c6f62cccf3a9721c6a2eb92d4
aamamun24 <bdmamaun100@gmail.com>
2023-10-21 20:09:34 +0600

    cart product delete form database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 
 const port = process.env.PORT || 5000;
@@ -56,8 +56,15 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id}
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
 
-        
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
